5c0e274849a39ddfcce45ea507f2c862268532d3
Gergely Marosi <marosige@gmail.com>
2025-01-30 15:32:53 +0100

    home fix

--- systems/unix/src/link_files.sh
+++ systems/unix/src/link_files.sh
@@ -5,5 +5,5 @@
 ###############################################################################
 
 # Call the linkdirectory script on the dotfiles directories
-bash "$IGNITION_ROOT/src/link_directory.sh" "$IGNITION_UNIX/dotfiles" "$$HOME"
-bash "$IGNITION_ROOT/src/link_directory.sh" "$IGNITION_OS/dotfiles" "$$HOME"
+bash "$IGNITION_ROOT/src/link_directory.sh" "$IGNITION_UNIX/dotfiles" "$HOME"
+bash "$IGNITION_ROOT/src/link_directory.sh" "$IGNITION_OS/dotfiles" "$HOME"
