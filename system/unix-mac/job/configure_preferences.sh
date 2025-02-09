3cb605f53d8278b316e5ce7eb1f4900778ab4c0e
Gergely Marosi <marosige@gmail.com>
2025-02-09 21:44:58 +0100

    debug messages

--- system/unix-mac/job/configure_preferences.sh
+++ system/unix-mac/job/configure_preferences.sh
@@ -5,4 +5,9 @@
 ###############################################################################
 
 # Run all preference scripts in the preferences folder
+
+echo "$IGNITION_INDENT Configuring system preferences..."
+
 lib_run_scripts_in_folder "$IGNITION_ACTIVE_SYSTEM/preferences"
+
+echo "$IGNITION_DONE System preferences configured"
