e6009c6449a685fd0dd1f7e1c6fe7d54edeacecb
Gergely Marosi <marosige@gmail.com>
2025-02-11 13:37:08 +0100

    configure fix

--- script/option_configure_preferences.sh
+++ script/option_configure_preferences.sh
@@ -8,6 +8,10 @@
 # Loop through each system directory
 for system_path in "$IGNITION_SYSTEM"/*; do
   if [[ -d "$system_path" ]]; then
+    export IGNITION_ACTIVE_SYSTEM="$system_path"
+    system_name=$(basename "$system_path")
+    echo -e "$IGNITION_TASK Configuring preferences for $system_name"
     bash "$IGNITION_ACTIVE_SYSTEM/job/configure_preferences.sh"
+    echo -e "$IGNITION_DONE $system_name preferences configured"
   fi
 done
