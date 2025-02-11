f4c98cbfd8c4231b35daf9a3413d37a03cf7d58c
Gergely Marosi <marosige@gmail.com>
2025-02-11 14:37:04 +0100

    menu shows all options

--- ignition.sh
+++ ignition.sh
@@ -15,32 +15,69 @@ if ! echo "$PULL_OUTPUT" | grep -q "Already up to date."; then
     exit
 fi
 
-# Show menu
-option_install="Install system with Ignition"
-option_configure_preferences="Configure system preferences"
-option_exit="Exit"
-
-# TODO when ignition is not installed yet, show only the install option and exit
-# TODO hide the install option when ignition is already installed
-options=(
-  "$option_install"
-  "$option_configure_preferences"
-  "$option_exit"
-)
-
 echo -e "$IGNITION_TITLE Ignition Main Menu"
+echo "$IGNITION_INDENT This script is configuring system settings and preferences."
+echo "$IGNITION_INDENT Please be aware that running this script will modify your system and may affect your existing configurations."
+echo "$IGNITION_INDENT Ensure you have reviewed the script and understand the changes it will make before proceeding: https://github.com/marosige/ignition"
+echo "$IGNITION_INDENT IMPORTANT: Backup any important configurations or data before running this script."
+echo "$IGNITION_INDENT It is an irreversible process. Once the setup is complete, reverting the changes may be challenging."
+
+# Show menu
+job_install="Install system with Ignition (all jobs)"
+job_update_system="Update system"
+job_create_directories="Create directories"
+job_link_files="Link files"
+job_install_packages="Install packages"
+job_configure_preferences="Configure system preferences"
+job_exit="Exit"
+
+# If ignition is not installed yet, only show the install option
+if [ -L "$HOME/bin/ignition" ]; then
+  options=(
+    "$job_install"
+    "$job_update_system"
+    "$job_create_directories"
+    "$job_link_files"
+    "$job_install_packages"
+    "$job_configure_preferences"
+    "$job_exit"
+  )
+else
+  ack
+  options=(
+    "$job_install"
+    "$job_exit"
+  )
+fi
+
 choice=$(lib_menu "${options[@]}")
 
 case "$choice" in
-  "$option_install")
-    bash "$IGNITION_ROOT/script/option_install.sh"
+  "$job_install")
+    bash "$IGNITION_ROOT/script/option_install.sh --all"
     echo -e "$IGNITION_DONE Setting up Ignition completed!"
     ;;
-  "$option_configure_preferences")
-    bash "$IGNITION_ROOT/script/option_configure_preferences.sh"
-    echo -e "$IGNITION_DONE Configuring Ignition preferences completed!"
+  "$job_update_system")
+    bash "$IGNITION_ROOT/script/option_install.sh --update-system"
+    echo -e "$IGNITION_DONE Updating system completed!"
     ;;
-  "$option_exit")
+  "$job_create_directories")
+    bash "$IGNITION_ROOT/script/option_install.sh --create-directories"
+    echo -e "$IGNITION_DONE Creating directories completed!"
+    ;;
+  "$job_link_files")
+    bash "$IGNITION_ROOT/script/option_install.sh --link-files"
+    echo -e "$IGNITION_DONE Linking files completed!"
+    ;;
+  "$job_install_packages")
+    bash "$IGNITION_ROOT/script/option_install.sh --install-packages"
+    echo -e "$IGNITION_DONE Installing packages completed!"
+    ;;
+  "$job_configure_preferences")
+    bash "$IGNITION_ROOT/script/option_install.sh --configure-preferences"
+    echo -e "$IGNITION_DONE Configuring preferences completed!"
+    ;;
+  "$job_exit")
     echo -e "$IGNITION_TASK Exiting..."
     ;;
 esac
