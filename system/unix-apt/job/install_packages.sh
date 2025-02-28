6e72089de39d85fbad58d3e3a278b9ce3cb089b1
Gergely Marosi <marosige@gmail.com>
2025-02-28 15:29:15 +0100

    install docker

--- system/unix-apt/job/install_packages.sh
+++ system/unix-apt/job/install_packages.sh
@@ -26,12 +26,6 @@ apt=(
     "git"             # Version control system
     "docker.io"       # Docker
     "openssh-server"  # OpenSSH server for remote access
-    "python3-pip"     # Python package installer
-)
-
-# List of pip packages to install
-pip=(
-    "docker-compose"  # Docker Compose via pip3
 )
 
 # Install APT packages
@@ -40,11 +34,22 @@ for app in "${apt[@]}"; do
     sudo apt install -y "$app"
 done
 
-# Install pip packages
-for app in "${pip[@]}"; do
-    echo -e "$IGNITION_TASK pip installing $app..."
-    sudo pip3 install --upgrade "$app"
-done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the Docker packages.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Clean up unnecessary packages and cache
 echo -e "$IGNITION_TASK Cleaning up..."
