71f8e94f7c3c449b68b4d493486d8af4ca2f7d46
Gergely Marosi <marosige@gmail.com>
2025-03-04 22:38:00 +0100

    install packages

--- system/unix-apt/job/install_packages.sh
+++ system/unix-apt/job/install_packages.sh
@@ -5,27 +5,29 @@
 # Update apt and install applications
 ###############################################################################
 
-echo "Installing dependencies..."
 apt=(
-  "fish"            # User-friendly command-line shell for UNIX-like operating systems
-  "mc"              # Midnight Commander: Terminal-based visual file manager
-  "tldr"            # Simplified and community-driven man pages
-  "thefuck"         # Programmatically correct mistyped console commands
-  "curl"            # Command line tool for transferring data
-  "wget"            # Internet file retriever
-  "nano"            # Text editor
-  "ufw"             # Uncomplicated Firewall
-  "vsftpd"          # FTP server
-  "ffmpeg"          # Play, record, convert, and stream audio and video
-  "youtube-dl"      # Download YouTube videos from the command-line
-  "speedtest-cli"   # Command-line interface for https://speedtest.net bandwidth tests
-  "btop"            # Resource monitor. C++ version and continuation of bashtop and bpytop
-  "neofetch"        # Fast, highly customisable system info script
-  "gum"             # Tool for glamorous shell scripts
-  "git"             # Version control system
-  "openssh-server"  # OpenSSH server for remote access
+    "fish"            # User-friendly command-line shell for UNIX-like operating systems
+    "mc"              # Midnight Commander: Terminal-based visual file manager
+    "tldr"            # Simplified and community-driven man pages
+    "thefuck"         # Programmatically correct mistyped console commands
+    "curl"            # Command line tool for transferring data
+    "wget"            # Internet file retriever
+    "nano"            # Text editor
+    "ufw"             # Uncomplicated Firewall
+    "vsftpd"          # FTP server
+    "ffmpeg"          # Play, record, convert, and stream audio and video
+    "youtube-dl"      # Download YouTube videos from the command-line
+    "speedtest-cli"   # Command-line interface for https://speedtest.net bandwidth tests
+    "btop"            # Resource monitor. C++ version and continuation of bashtop and bpytop
+    "neofetch"        # Fast, highly customisable system info script
+    "gum"             # Tool for glamorous shell scripts
+    "git"             # Version control system
+    "openssh-server"  # OpenSSH server for remote access
+    "docker.io"       # Docker - Official package for Docker from Ubuntu repositories
+    "docker-compose"  # Docker Compose - from Ubuntu repositories (not the latest)
 )
-sudo apt install -y "${apt[@]}"
 
-echo "Installing Docker..."
-apt install -y docker.io docker-compose
+for app in "${apt[@]}"; do
+    echo -e "$IGNITION_TASK apt installing $app..."
+    sudo apt install -y "$app"
+done
