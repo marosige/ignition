530def9606b93d65c1e54f90e73c641d808b607f
Gergely Marosi <marosige@gmail.com>
2025-03-05 01:00:56 +0100

    fish fix

--- system/unix-apt/preferences/fish.sh
+++ system/unix-apt/preferences/fish.sh
@@ -4,21 +4,11 @@
 ###############################################################################
 # Fish
 ###############################################################################
-exit=0
 
-addIfMissing () {
-  # Add Fish to /etc/shells if it’s not already listed
-  if ! grep -wq "$1" /etc/shells; then
-      echo "$1" | sudo tee -a /etc/shells
-  fi
-}
+# Ensure Fish is in /etc/shells
+if ! grep -wq "/usr/bin/fish" /etc/shells; then
+    echo "/usr/bin/fish" | sudo tee -a /etc/shells
+fi
 
-# Add fish into /etc/shells if missing
-addIfMissing "/usr/bin/fish" || exit=1
-
-echo -e "$IGNITION_TASK Setting Fish as default shell..."
-
-# Set fish as default shell for the current user
-sudo chsh -s "$(which fish)" >/dev/null 2>&1 || exit=1
-
-exit $exit
+# Set fish as the default shell for the current user
+chsh -s /usr/bin/fish $USER
