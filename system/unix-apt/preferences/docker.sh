bf083ebfd99433373b16d502b1f48adbb4e1cb98
Gergely Marosi <marosige@gmail.com>
2025-03-05 13:54:09 +0100

    docker group

--- system/unix-apt/preferences/docker.sh
+++ system/unix-apt/preferences/docker.sh
@@ -10,3 +10,9 @@ sudo systemctl enable --now docker
 
 echo -e "$IGNITION_TASK Starting Docker Compose..."
 sudo docker-compose -f "$HOME/docker/docker-compose.yml" up -d
+
+if ! groups $USER | grep -q "\bdocker\b"; then
+    echo -e "$IGNITION_TASK Adding $USER to docker group..."
+    sudo usermod -aG docker $USER
+    echo -e "$IGNITION_WARN Please log out and log back in to apply the group changes."
+fi
