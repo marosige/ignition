8be7e8f42646ebaa813310963817532d847f2ae0
Gergely Marosi <marosige@gmail.com>
2025-04-09 16:04:57 +0200

    homebrew fix

--- system/unix-mac/install_packages.sh
+++ system/unix-mac/install_packages.sh
@@ -7,13 +7,25 @@
 
 echo -e "$IGNITION_TASK Installing Homebrew Applications..."
 
-# Install Homebrew if not installed - brew.sh
+# Request sudo access upfront
+sudo -v
+while true; do sudo -n true; sleep 60; done 2>/dev/null &
+KEEP_ALIVE_PID=$!
+trap 'kill "$KEEP_ALIVE_PID"' EXIT
+
+# Install Homebrew if not installed
 if ! hash brew 2>/dev/null; then
   echo -e "$IGNITION_TASK Homebrew not found. Installing..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  
+  # On M1 Macs, Homebrew installs to /opt/homebrew, so we directly use it.
+  if [[ -d "/opt/homebrew/bin" ]]; then
+    # Source Homebrew immediately, in case this is run without restarting the shell
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 fi
 
-# Make sure we are using the latest Homebrew
+# Ensure Homebrew is up-to-date
 brew update
 
 # Upgrade existing packages
@@ -27,5 +39,5 @@ for brewfile in "$HOME"/Brewfile.*; do
   fi
 done
 
-# Remove outdated versions from the cellar including casks
+# Clean up outdated versions from the cellar, including casks
 brew cleanup
